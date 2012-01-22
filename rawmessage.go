3449ce7717832a83585f196ed4acf5e83f77c4f3
Matej Knific <norisatir@gmail.com>
2012-01-22 17:19:58 +0100

    Added constants for dbus types. Moved methods from rawMessage to rawHeader.

--- rawmessage.go
+++ rawmessage.go
@@ -1,5 +1,47 @@
 package dbus
 
+// Types
+const (
+	Byte = 'y'
+	Boolean = 'b'
+	Int16 = 'n'
+	UInt16 = 'q'
+	Int32 = 'i'
+	UInt32 = 'u'
+	Int64 = 'x'
+	UInt64 = 't'
+	Double = 'd'
+	String = 's'
+	ObjectPath = 'o'
+	Signature = 'g'
+	Array = 'a'
+	Variant = 'v'
+	StructBegin = '('
+	StructEnd = ')'
+	DictBegin = '{'
+	DictEnd = '}'
+)
+
+// Alignments of above types
+var alignment map[byte]int = map[byte]int {
+	Byte : 1,
+	Boolean : 4,
+	Int16 : 2,
+	UInt16 : 2,
+	Int32 : 4,
+	UInt32 : 4,
+	Int64 : 8,
+	UInt64 : 8,
+	Double : 8,
+	String : 4,
+	ObjectPath : 4,
+	Signature : 1,
+	Array : 4,
+	StructBegin : 8,
+	Variant : 1,
+	DictBegin : 8,
+}
+
 // Offset of individual data of message signature
 const (
 	rawMsgTypeOffset = 1
@@ -73,6 +115,42 @@ type rawHeader struct {
 	HeaderSignature []byte
 	Fields []byte
 }
+
+func (self *rawHeader) Type() MessageType {
+	t,_ := _GetByte(self.HeaderSignature, rawMsgTypeOffset)
+	return MessageType(t)
+}
+
+func (self *rawHeader) Flags() MessageFlag {
+	f,_ := _GetByte(self.HeaderSignature, rawMsgFlagsOffset)
+	return MessageFlag(f)
+}
+
+func (self *rawHeader) ProtocolVersion() int {
+	pv,_ := _GetByte(self.HeaderSignature, rawMsgVersionOffset)
+	return int(pv)
+}
+
+func (self *rawHeader) BodyLength() int {
+	bl,_ := _GetInt32(self.HeaderSignature, rawMsgBodyLengthOffset)
+	return int(bl)
+}
+
+func (self *rawHeader) Serial() int {
+	serial,_ := _GetInt32(self.HeaderSignature, rawMsgSerialOffset)
+	return int(serial)
+}
+
+func (self *rawHeader) FieldsArrayLength() int {
+	length,_ := _GetInt32(self.Fields, 0)
+	return int(length)
+}
+
+func (self *rawHeader) PaddingBeforeBody() int {
+	headerLen := MinimumHeaderSize + self.FieldsArrayLength()
+	return (_Align(8, headerLen) - headerLen)
+}
+
 type rawMessage struct {
 	Header *rawHeader
 	Body []byte
@@ -123,40 +201,6 @@ func newRawMessage(sig []byte) (*rawMessage, error) {
 	return r, nil
 }
 
-func (self *rawMessage) Type() MessageType {
-	t,_ := _GetByte(self.Header.HeaderSignature, rawMsgTypeOffset)
-	return MessageType(t)
-}
-
-func (self *rawMessage) Flags() MessageFlag {
-	f,_ := _GetByte(self.Header.HeaderSignature, rawMsgFlagsOffset)
-	return MessageFlag(f)
-}
-
-func (self *rawMessage) ProtocolVersion() int {
-	pv,_ := _GetByte(self.Header.HeaderSignature, rawMsgVersionOffset)
-	return int(pv)
-}
-
-func (self *rawMessage) BodyLength() int {
-	bl,_ := _GetInt32(self.Header.HeaderSignature, rawMsgBodyLengthOffset)
-	return int(bl)
-}
-
-func (self *rawMessage) Serial() int {
-	serial,_ := _GetInt32(self.Header.HeaderSignature, rawMsgSerialOffset)
-	return int(serial)
-}
-
-func (self *rawMessage) FieldsArrayLength() int {
-	length,_ := _GetInt32(self.Header.Fields, 0)
-	return int(length)
-}
-
-func (self *rawMessage) PaddingBeforeBody() int {
-	headerLen := MinimumHeaderSize + self.FieldsArrayLength()
-	return (_Align(8, headerLen) - headerLen)
-}
 
 func (self *rawMessage) FieldsAndBodySlice() []byte {
 	return self.Msg[MinimumHeaderSize:]
